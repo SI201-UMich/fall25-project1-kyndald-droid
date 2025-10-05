4b222f2d82249d3d8047766176669b3718b260fe
Kyndal Duncan <kyndalduncan@MacBook-Pro-81.local>
2025-10-05 16:14:38 -0400

    Imported csv and created the first function to read the CSV into a list of dictionaries

--- project1.py
+++ project1.py
@@ -1,3 +1,16 @@
 ### code for project 1
 ### Kyndal D
+### ID: 85174585
+### kyndald@umich.edu
+### Gen AI used for debugging and helping with parts of code and structure
 ### Fall 2025
+### Dataset: Penguins (from Kaggle)
+
+import csv
+
+# Function 1: Read CSV into a list of dictionaries
+def read_csv(filename):
+    with open(filename, newline='') as csvfile:
+        reader = csv.DictReader(csvfile)
+        data = [row for row in reader]
+    return data
